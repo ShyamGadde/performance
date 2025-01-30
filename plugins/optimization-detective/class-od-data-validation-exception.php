c21bbf369fd950f9c6573890cf7d739dcdfa4389
Shyamsundar Gadde <shyamsundar.gadde@rtcamp.com>
2025-01-31 00:55:26 +0530

    Add spacing and code coverage ignore comments

--- plugins/optimization-detective/class-od-data-validation-exception.php
+++ plugins/optimization-detective/class-od-data-validation-exception.php
@@ -6,11 +6,13 @@
  * @since 0.1.0
  */
 
+
 // @codeCoverageIgnoreStart
 if ( ! defined( 'ABSPATH' ) ) {
 	exit; // Exit if accessed directly.
 }
 // @codeCoverageIgnoreEnd
+
 
 /**
  * Exception thrown when failing to validate URL Metrics data.
